0c6414d8a72d4dc50e9d18f67f3fcecf3936aeaf
Cu1 <cu1universe@163.com>
2023-10-18 23:37:09 +0800

    init

--- ddd-archetype-launcher/src/main/java/com/feiniaojin/ddd/launcher/Launcher.java
+++ ddd-archetype-launcher/src/main/java/com/feiniaojin/ddd/launcher/Launcher.java
@@ -2,8 +2,10 @@ package com.feiniaojin.ddd.launcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Launcher {
     public static void main(String[] args) {
         SpringApplication.run(Launcher.class, args);
